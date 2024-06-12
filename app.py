dd8b2fa376e20aa45f9487b4df369bd5d5bde5d7
Luana De Oliveira Sousa <luana.sousa@shopee.com>
2024-06-12 12:57:12 -0300

    Adicionando logs para depuração de IP e verificando ALLOWED_IPS

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import os
 from googleapiclient.discovery import build
 from google.oauth2.service_account import Credentials
 
-
 app = Flask(__name__)
 
 # Obtém os IPs permitidos das variáveis de ambiente
@@ -13,7 +12,6 @@ print(f"Allowed IPs: {ALLOWED_IPS}")
 
 @app.before_request
 def limit_remote_addr():
-    # Render armazena o endereço IP real do cliente no cabeçalho 'X-Forwarded-For'
     if 'X-Forwarded-For' in request.headers:
         user_ip = request.headers['X-Forwarded-For'].split(',')[0].strip()
     else:
@@ -21,11 +19,9 @@ def limit_remote_addr():
 
     print(f"Cliente IP: {user_ip}")
 
-
-    # Verifica se o IP está na lista de IPs permitidos
     if user_ip not in ALLOWED_IPS:
         print(f"Acesso negado para IP: {user_ip}")
-        abort(403)  # Se o IP não estiver na lista, proíbe o acesso
+        abort(403)
     else:
         print(f"Acesso permitido para IP: {user_ip}")
 
@@ -42,12 +38,9 @@ SPREADSHEET_ID = '1144zWwPY4atubZ--ZtIabpMeqeIj_0uMOoVXCfPTyTQ'
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        # Obter valores do formulário
         nome = request.form.get('nome')
         action = request.form.get('action')
-        # Ler a data e hora atual
         now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        # Escrever no Google Sheets
         range_ = 'Registros!A2:C' 
         values = [
             [now, nome, action]
@@ -61,11 +54,9 @@ def home():
 
         return redirect(url_for('home'))
     else:
-        # Obter nomes da planilha
         result = service.spreadsheets().values().get(
             spreadsheetId=SPREADSHEET_ID, range='Cadastro!A2:A').execute()
         nomes = result.get('values', [])
-        # Transforma em uma lista simples
         nomes = [nome[0] for nome in nomes if nome]
         return render_template('index.html', nomes=nomes, current_time=datetime.datetime.now())
 
